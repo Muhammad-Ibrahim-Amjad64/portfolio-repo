31f464b503832e3045c6e36ba87e7d6e592319af
Muhammad Ibrahim <123ibrahim7890@gmail.com>
2023-08-12 02:10:37 +0500

    added something

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -1,47 +1,56 @@
 import React from "react";
 import Button from "../Button";
 
-
-const WorkCard = ({ img, name, description, onClick, descriptionLarge, skills }) => {
+const WorkCard = ({
+  img,
+  name,
+  description,
+  onClick,
+  descriptionLarge,
+  skills,
+}) => {
   // console.log(img)
   // const imagePath = require("../../assets/redux cart 1.png").default
-  let title = "View Project"
+  let title = "View Project";
   if (description === "Mobile App Development") {
-    title="Download APK"
-    
+    title = "Download APK";
   }
   return (
-    <div 
-    // <div style={{ fontFamily: 'Hind' }}
-      className=" rounded-lg ph-2  first:ml-0 link bg-slate-800 hover:bg-black duration-300 p-3 "
-     
+    <div
+      // <div style={{ fontFamily: 'Hind' }}
+      className=" rounded-lg ph-2  first:ml-0 link bg-slate-800 hover:bg-black duration-300 p-3  flex flex-col  justify-between "
     >
-      <div
-        className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto bg-white h-500 w-768"
-        style={{ height: "250px" }}
-      >
-        <img
-          style={{backgroundColor:"white"}}
-          alt={name}
-          className="h-full  w-full object-contain bg-slate-800 hover:scale-110 transition-all ease-out duration-300 overflow-hidden"
-          src={img}
-       
-
-        ></img>
-      </div>
-      <h1 className="mt-5 text-3xl font-medium text-slate-400 ">
-        {name ? name : "Project Name"}
-      </h1>
-      <h2 className="text-xl opacity-50 " font-sans font-semibold>
-        {description ? description : "Description"}
-      </h2>
-      <h2 className="font-sans font-semibold">{descriptionLarge}
-      </h2>
-      <h2 className="text-purple-300">{skills}
-        <div>
-          <Button onClick={onClick} type="primary">{ title}</Button>
+      <div >
+        <div
+          className="relative rounded-lg overflow-hidden transition-all ease-out duration-300 h-48 mob:h-auto bg-white h-500 w-768"
+          style={{ height: "250px" }}
+        >
+          <img
+            style={{ backgroundColor: "white" }}
+            alt={name}
+            className="h-full  w-full object-contain bg-slate-800 hover:scale-110 transition-all ease-out duration-300 overflow-hidden"
+            src={img}
+          ></img>
         </div>
-      </h2>
+        <h1 className="mt-5 text-3xl font-medium text-slate-400 ">
+          {name ? name : "Project Name"}
+        </h1>
+        <h2 className="text-xl opacity-50 " font-sans font-semibold>
+          {description ? description : "Description"}
+        </h2>
+        <h2 className="font-sans font-semibold">{descriptionLarge}</h2>
+      </div>
+      {/* child 1  */}
+      <div className="my-4">
+        <h2 className="text-purple-300">
+          {skills}
+          <div>
+            <Button onClick={onClick} type="primary">
+              {title}
+            </Button>
+          </div>
+        </h2>
+      </div>
     </div>
   );
 };
